93eaebe3a4930da4370782cf6107f26f89b48e11
Matt Grogan <mg185230@ncr.com>
2016-07-15 14:01:50 -0400

    init

---
+++ SchedulingTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+
+namespace SchedulingTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(
+                "Data Source=(local);Initial Catalog=HangfireTest;Integrated Security=True");
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.WriteLine();
+                for (var i = 0; i < 1000; i++)
+                {
+                    RecurringJob.AddOrUpdate($"test-job-{i}", () => Execute(), Cron.Minutely);
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        public static void Execute()
+        {
+            var now = DateTime.Now;
+            var machineName = Environment.MachineName;
+            Console.WriteLine($"Yo! {machineName} -- {now}");
+        }
+    }
+}
